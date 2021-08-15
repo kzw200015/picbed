b46dcedf139b91c6de59f80f2edd27d153e46b85
kzw200015 <kzw200015@gmail.com>
2021-08-15 20:39:02 +0800

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,8 @@ FROM alpine:latest
 
 COPY --from=go-builder /build/picbed /app/picbed
 
-RUN mkdir -p /etc/picbed && touch /etc/picbed/config.yml && echo "imgSrc: /data" >> /etc/picbed/config.yml
+RUN mkdir -p /etc/picbed && touch /etc/picbed/config.yml \
+    && echo "imgSrc: /data" >> /etc/picbed/config.yml
 
 VOLUME /etc/picbed/config.yml
 
--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/kzw200015/picbed/config"
 	"github.com/kzw200015/picbed/routes"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := routes.Get()
 	e.Debug = config.Get().Debug
+	e.Logger.SetLevel(log.DEBUG)
 	e.HideBanner = true
 	e.Logger.SetHeader("[${time_rfc3339}] ${level}")
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(config.Get().Port)))
